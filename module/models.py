e16e8eb5530e885af233946217b1fe989b9e9764
bio-25 <kunalnandwana1999@gmail.com>
2021-01-12 14:21:52 +0530

    first commit

---
+++ module/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+# Create your models here.
+class patient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=75)
+    email = models.EmailField()
+    gender = models.CharField(max_length=1)
+    address = models.TextField()
+    mobile = models.BigIntegerField()
+
+
+class doctor(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=75)
+    email = models.EmailField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    address = models.TextField()
+    mobile = models.BigIntegerField()
+    specialization = models.TextField()
+
+
+class receptionist(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=75)
+    email = models.EmailField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    hospital = models.TextField()
+
+class appointments(models.Model):
+    id =models.AutoField(primary_key=True)
+    patient = models.ForeignKey('patient',on_delete=models.CASCADE)
+    doctor = models.ForeignKey('doctor',on_delete=models.CASCADE)
+    slot_date = models.DateField()
+    slot_time = models.TimeField(blank=True,null=True)
+    createdAt = models.DateField()
+    updatedAt = models.DateField()
+    status = models.CharField(max_length=10)
+    prescription = models.CharField(max_length=2000,blank=True)
+
+class labReports(models.Model):
+    patient = models.ForeignKey('patient',on_delete=models.CASCADE)
+    reports = models.FileField(upload_to=None)
+    upload_date = models.DateField()
+    upload_time = models.TimeField()
+    doneBy = models.ForeignKey('laboratory',on_delete=models.CASCADE)
+
+class laboratory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    address = models.CharField(max_length=30)
+    contact_no  = models.BigIntegerField()
+
+class timeslot(models.Model):
+    id = models.AutoField(primary_key=True)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    doctor = models.ForeignKey('doctor',on_delete=models.CASCADE)
+    isActive = models.BooleanField()
